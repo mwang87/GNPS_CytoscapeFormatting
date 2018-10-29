cad9f14a47ee405dd87d3ae2932c74e38fed1876
Ming Wang <mwang87@gmail.com>
2018-10-29 16:02:45 -0700

    Setting memory max

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 docker rm gnpscytoscape
-docker run -d -p 5051:5051 -p 1234:1234 --name gnpscytoscape gnpscytoscape /app/run_server_in_docker.sh
-#docker run -p 5051:5051 -p 1234:1234 --name gnpscytoscape gnpscytoscape /app/run_server_in_docker.sh
+docker run -d -p 5051:5051 --memory=20G -p 1234:1234 --name gnpscytoscape gnpscytoscape /app/run_server_in_docker.sh
+#docker run -p 5051:5051 --memory=20G -p 1234:1234 --name gnpscytoscape gnpscytoscape /app/run_server_in_docker.sh
