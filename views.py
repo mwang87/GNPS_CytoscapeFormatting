a0e63bf23b8773f4c56f35a73192a2d102996412
Mingxun Wang <mwang87@gmail.com>
2019-06-24 09:18:02 +0200

    making mapping work

--- views.py
+++ views.py
@@ -27,9 +27,7 @@ def heartbeat():
 def process():
     taskid = request.args["task"]
 
-    query_url = "/process?task=%s" % (taskid)
-
-    query_parameters = {}
+    query_parameters = {"task" : taskid}
 
     return render_template("process.html", task=taskid, query_parameters=query_parameters)
 
@@ -37,8 +35,6 @@ def process():
 def metabotracker_view():
     taskid = request.args["task"]
     source = request.args["source"]
-
-    #query_url = "/process?task=%s&source=%s&filter=tagtracker" % (taskid, metabotracker)
 
     query_parameters = {"task" : taskid, "filter": "tagtracker", "source": source}
 
@@ -49,7 +45,6 @@ def molnetenhancer_view():
     taskid = request.args["task"]
     molnetenhancer_class = request.args["molnetenhancer_class"]
 
-    #query_url = "/process?task=%s&molnetenhancer_class=%s&filter=molnetenhancer" % (taskid, molnetenhancer_class)
     query_parameters = {"task" : taskid, "filter": "molnetenhancer", "molnetenhancer_class": molnetenhancer_class}
 
     return render_template("process.html", task=taskid, query_parameters=query_parameters)
@@ -87,15 +82,20 @@ def get_graph_object(taskid):
     local_file.write(requests.get(url_to_graph).text)
     local_file.close()
 
-    return local_filepath
+    return local_filepath, task_status["workflow"]
+
 
 @app.route('/process', methods=['POST'])
 def process_ajax():
     print(request.values)
-    #PORT_NUMBER = 1234
 
+    style_filename = "Styles/Sample2.json"
     taskid = request.values["task"]
-    local_filepath = get_graph_object(taskid)
+    local_filepath, workflow_name = get_graph_object(taskid)
+
+    #Defining style given the type of data
+    if workflow_name == "MOLNETENHANCER":
+        style_filename = "Styles/MolnetEnhancer_ChemicalSuperClasses.json"
 
     if "filter" in request.values:
         if request.values["filter"] == "tagtracker":
@@ -106,6 +106,7 @@ def process_ajax():
         if request.values["filter"] == "molnetenhancer":
             print("Molnetenhancer")
             metabotracker.molnetenhancer_wrapper(local_filepath, local_filepath, class_header="CF_class", class_name=request.values["molnetenhancer_class"])
+            style_filename = "Styles/MolnetEnhancer_ChemicalSuperClasses.json"
 
     cytoscape_process = subprocess.Popen("Cytoscape", shell=True)
 
@@ -113,6 +114,7 @@ def process_ajax():
     while(1):
         try:
             cy  = CyRestClient()
+            print("Success Cyrest")
             #new_cy = cyrest(port=PORT_NUMBER)
             break
         except:
@@ -125,18 +127,34 @@ def process_ajax():
 
     mystyle = cy.style.create("ClassDefault")
 
-    #Loading style
-    all_parameters = json.loads(open("Styles/Sample2.json").read())
-
+    """Loading style"""
+    all_parameters = json.loads(open(style_filename).read())
 
     new_defaults_dict = {}
     for item in all_parameters["defaults"]:
         new_defaults_dict[item["visualProperty"]] = item["value"]
 
+    #Loading the passthrough mapping
     mappings_list = all_parameters["mappings"]
     for mapping in mappings_list:
         if mapping["mappingType"] == "passthrough":
             mystyle.create_passthrough_mapping(column=mapping["mappingColumn"], col_type=mapping["mappingColumnType"], vp=mapping["visualProperty"])
+
+    #Loading the descrete mapping
+    for mapping in mappings_list:
+        if mapping["mappingType"] == "discrete":
+            reformatted_mapping = {}
+            for item in mapping["map"]:
+                reformatted_mapping[item["key"]] = item["value"]
+            mystyle.create_discrete_mapping(column=mapping["mappingColumn"], col_type=mapping["mappingColumnType"],
+                                vp=mapping["visualProperty"], mappings=reformatted_mapping)
+
+    #Loading a continuous mapping, TODO: need to debug
+    for mapping in mappings_list:
+        if mapping["mappingType"] == "continuous":
+            mystyle.create_continuous_mapping(column=mapping["mappingColumn"], col_type=mapping["mappingColumnType"],
+                                  vp=mapping["visualProperty"], points=mapping["points"])
+
 
     mystyle.update_defaults(new_defaults_dict)
 
