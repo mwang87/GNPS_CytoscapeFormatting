e526349805417dc3447d6605acd4262f287cb87d
Mingxun Wang <mwang87@gmail.com>
2020-05-18 12:41:12 -0700

    updating style

--- code/Styles/generate_style.py
+++ code/Styles/generate_style.py
@@ -1,13 +1,11 @@
 """Lets try to get all visual properties and save to json files ourselves"""
 
-from py2cytoscape import cyrest
 import sys
 import json
+import requests
 
-cytoscape=cyrest.cyclient()
-cytoscape.version()
-cytoscape.network.list()
+local_url = "http://localhost:1234/v1/styles/{}".format(sys.argv[1])
 
-full_style_json = cytoscape.styles.getStyleFull(sys.argv[1], verbose=True).json()
+full_style_json = requests.get(local_url).json()
 
 open(sys.argv[2], "w").write(json.dumps(full_style_json, indent=4))
